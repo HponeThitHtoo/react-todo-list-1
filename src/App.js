9a99e75b5eea0dcce428e2f9661f40baf2a16d07
HponeThitHtoo <white.nuzzle@gmail.com>
2020-08-13 21:28:39 +0630

    handleChange and handleSubmit events

--- src/App.js
+++ src/App.js
@@ -18,14 +18,35 @@ class App extends Component {
     editItem: false,
   };
 
-  handleChange = (e) => { console.log('handle change') }
-  handleSubmit = (e) => { console.log('handle submit') }
+  handleChange = (e) => { 
+    this.setState({
+      item: e.target.value,
+    });
+  }
+
+  handleSubmit = (e) => { 
+    e.preventDefault();
+
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item,
+    };
+
+    const updatedItems = [...this.state.items, newItem];
+
+    this.setState({
+      items: updatedItems,
+      item: '',
+      id: uuidv4(),
+      editItem: false,
+    });
+  }
+
   clearList = () => { console.log('clear list') }
   handleDelete = (id) => { console.log(`handle edit ${id}`) }
   handleEdit = (id) => { console.log(`handle edit ${id}`) }
 
   render() {
-    console.log(this.state);
 
     return (
       <div>
